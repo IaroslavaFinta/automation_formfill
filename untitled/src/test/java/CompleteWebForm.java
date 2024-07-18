0f3557990afa68d88c22c853bca1d7322c8094dc
Iaroslava <Iaroslava@MacBook-Pro.local>
2024-07-17 20:40:37 -0500

    Removed tread sleep from Complete form

--- untitled/src/test/java/CompleteWebForm.java
+++ untitled/src/test/java/CompleteWebForm.java
@@ -18,52 +18,41 @@ public class CompleteWebForm {
         WebElement firstName = driver.findElement(By.id("first-name"));
         firstName.sendKeys("Iaroslava");
         firstName.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
 
 //       #last name
         WebElement lastName = driver.findElement(By.id("last-name"));
         lastName.sendKeys("Finta");
         lastName.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
 
 //        #job title
         WebElement jobTitle = driver.findElement(By.id("job-title"));
         jobTitle.sendKeys("tester");
         jobTitle.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
 
 //        #highest level of education
         WebElement highSchool = driver.findElement(By.id("radio-button-1"));
         highSchool.click();
-        Thread.sleep(2000);
 
 //        #sex
         WebElement sexFemale = driver.findElement(By.id("checkbox-2"));
         sexFemale.click();
-        Thread.sleep(2000);
 
 //        #years of experience
         WebElement yearsOfExperience = driver.findElement(By.id("select-menu"));
         yearsOfExperience.click();
-        Thread.sleep(1000);
 
         WebElement firstOption = driver.findElement(By.xpath("/html/body/div/form/div/div[6]/select/option[2]"));
         firstOption.click();
-        Thread.sleep(3000);
 
 //        #date
         WebElement date = driver.findElement(By.id("datepicker"));
         date.sendKeys("07/16/2024");
-        Thread.sleep(2000);
         date.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
 
 //        #submit button
         WebElement submitButton = driver.findElement(By.xpath("/html/body/div/form/div/div[8]/a"));
         submitButton.click();
-        Thread.sleep(3000);
 
         driver.quit();
-
     }
 }
